6d93f5777ec55dd7c4745f7c0b5810a5a18bf75d
Mythreyee Sammeta <mythreyee.sammeta@pearson.com>
2015-05-15 14:38:18 -0500

    Read mysql credentials from encrypted data bags

--- cookbooks/wordpress_db/recipes/default.rb
+++ cookbooks/wordpress_db/recipes/default.rb
@@ -8,10 +8,13 @@
 include_recipe 'mysql2_chef_gem'
 include_recipe 'mysql::server'
 
+mysql_secret = Chef::EncryptedDataBagItem.load_secret("/vagrant/.chef/encrypted_data_bag_secret")
+mysql_creds = Chef::EncryptedDataBagItem.load("mysql", "passwords", mysql_secret)
+
 mysql_connection_info = {
   :host => 'localhost',
   :username => 'root',
-  :password => 'password'
+  :password => mysql_creds['root']
 }
 
 mysql_database node['wordpress']['db']['database'] do
@@ -21,14 +24,14 @@ end
 
 mysql_database_user node['wordpress']['db']['user'] do
   connection mysql_connection_info
-  password node['wordpress']['db']['password']
+  password mysql_creds["#{node['wordpress']['db']['user']}"]
   action :create
 end
 
 %W{ % localhost }.each do |h|
   mysql_database_user node['wordpress']['db']['user'] do
     connection mysql_connection_info
-    password node['wordpress']['db']['password']
+    password mysql_creds["#{node['wordpress']['db']['user']}"]
     database_name node['wordpress']['db']['database']
     host h
     privileges [:all]
